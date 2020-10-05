60fd85f3d002ddb0720883ba600f8bd25575780d
skmahato <souravkumar1329@gmail.com>
2020-10-05 16:34:55 +0530

    margin added

--- src/components/Home/Authentication/styles.js
+++ src/components/Home/Authentication/styles.js
@@ -12,7 +12,7 @@ const styles = theme => ({
     },
   
     paper: {
-      marginTop: theme.spacing(8),
+      marginTop: 2,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
