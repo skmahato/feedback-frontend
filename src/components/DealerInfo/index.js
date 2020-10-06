37c14d6dcca01e09545e3f60a2a5853eac8eff88
Sourav Kumar Mahato <33520110+skmahato@users.noreply.github.com>
2020-10-06 16:47:39 +0530

    Priya/hack (#5)
    * add visible field
    
    * add style

--- src/components/DealerInfo/index.js
+++ src/components/DealerInfo/index.js
@@ -1,15 +1,23 @@
+import { blueGrey, green, grey, lightBlue } from '@material-ui/core/colors';
+import { flowRight } from 'lodash';
 import React from 'react';
+
+var divStyle = {
+    backgroundColor: "whitesmoke",
+    border : "solid grey",
+    marginBottom: "5px",
+    padding: 5,
+}
 
 export const DealerInfo = ({ dealer, handleDealerClick }) => {
     return (
-        <div>
+        <div style={divStyle}>
             <strong>{dealer.name}</strong> | {dealer.location}
             <p>{dealer.description}</p>
             <p>{dealer.phone} | {dealer.email}</p>
             <em>{dealer.website}</em>
-            <br />
-            {handleDealerClick && <button type="button" onClick={() => handleDealerClick(dealer.id)}>Reviews</button>}
-            <hr />
+            <br />          
+            {handleDealerClick && <button type="button" onClick={() => handleDealerClick(dealer.id)} style={{backgroundColor: "grey"}}>Reviews</button>}
         </div>
     )
 }
