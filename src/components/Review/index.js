e64fd7abf20016c351886af0881c28a6c0c96841
Vijeta Sobhwani <VSobhwani@dealersocket.com>
2020-10-06 13:25:13 +0530

    Beautyfying reviewsPage

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,48 +1,88 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button } from '@material-ui/core';
-
-import ReactForm from './ReviewForm';
-import { createReview } from '../../actions/reviews';
-import { dealershipReviewSelector } from '../../selectors/dealerships';
-import currentUser from '../../reducers/currentUser';
-import { isEmpty } from 'lodash';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "@material-ui/core";
+import ReactForm from "./ReviewForm";
+import { createReview } from "../../actions/reviews";
+import { dealershipReviewSelector } from "../../selectors/dealerships";
+import { isEmpty } from "lodash";
 
 export const Review = ({ selectedDealerId, currentUser }) => {
-    const dispatch = useDispatch();
-    const [open, setOpen] = useState(false);
-    const dealerReviews = useSelector(state => dealershipReviewSelector(state, selectedDealerId));
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const dealerReviews = useSelector((state) =>
+    dealershipReviewSelector(state, selectedDealerId)
+  );
 
-    const handleClick = () => setOpen(!open);
+  const handleClick = () => setOpen(!open);
 
-    const handleSubmit = (params) => {
-        dispatch(createReview(params, selectedDealerId)).then(res => {
-            console.log(res);
-        })
-    }
+  const handleSubmit = (params) => {
+    dispatch(createReview(params, selectedDealerId)).then((res) => {
+      console.log(res);
+    });
+  };
 
-    return (
-        <div>
-            {!isEmpty(currentUser) && (
-                <>
-                    <Button type="button" onClick={handleClick}>{open ? 'Cancel' : 'New Review'}</Button>
-                    {open && <ReactForm handleFormSubmit={handleSubmit} />}
+  return (
+    <div style={{ ...styles.ReviewsRectangleBox }}>
+      {!isEmpty(currentUser) && (
+        <>
+          <Button type="button" onClick={handleClick}>
+            {open ? "Cancel" : "New Review"}
+          </Button>
+          {open && <ReactForm handleFormSubmit={handleSubmit} />}
 
-                    <hr />
-                </>
-            )}
+          <hr />
+        </>
+      )}
 
-            {dealerReviews.map(f => {
-                return (
-                    <div key={f.id}>
-                        <strong>{f.title}</strong> | {f.rating} stars
-                        <p><em>{f.comment}</em></p>
-                        <hr />
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+      {dealerReviews.map((f) => {
+        return (
+          <div key={f.id}>
+            <p style={{ ...styles.MainTitleStyle }}>
+              <i> {f.title}</i> | {f.rating} stars
+            </p>
+            <p>
+              <em style={{ ...styles.commentStyle }}>{f.comment}</em>
+            </p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const styles = {
+  commentStyle: {
+    width: 800,
+    height: 20,
+    fontFamily: "Roboto",
+    fontSize: 15,
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    lineHeight: 1.54,
+    letterSpacing: 0.1,
+    color: "#333333",
+  },
+  MainTitleStyle: {
+    width: 800,
+    height: 22,
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "bold",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    lineHeight: 1.38,
+    letterSpacing: "normal",
+    color: "#1d86b8",
+  },
+  ReviewsRectangleBox: {
+    width: 600,
+    backgroundColor: "#e6e6e6",
+    boxSizing: "inherit",
+    padding: "1px 1px 1px 20px",
+    border: "solid 1px #e6e6e6",
+  },
+};
 
 export default Review;
