622add6208cdf3c4c7833769cd37fe388058211a
priyankabhadra <priyankabhadra18896@gmail.com>
2020-10-06 14:28:58 +0530

    add style

--- src/components/DealerInfo/index.js
+++ src/components/DealerInfo/index.js
@@ -1,15 +1,24 @@
+import { blueGrey, green, grey, lightBlue } from '@material-ui/core/colors';
+import { flowRight } from 'lodash';
 import React from 'react';
+
+var divStyle = {
+    backgroundColor: "whitesmoke",
+    border : "solid grey",
+    marginBottom: "5px",
+    padding: 5,
+}
 
 export const DealerInfo = ({ dealer, handleDealerClick }) => {
     return (
-        <div>
+        <div style={divStyle}>
             <strong>{dealer.name}</strong> | {dealer.location}
             <p>{dealer.description}</p>
             <p>{dealer.phone} | {dealer.email}</p>
             <em>{dealer.website}</em>
             <br />
-            <button type="button" onClick={() => handleDealerClick(dealer.id)}>Reviews</button>
-            <hr />
+            <button type="button" onClick={() => handleDealerClick(dealer.id)} style={{backgroundColor: "grey"}}>Reviews</button>
+          
         </div>
     )
 }
