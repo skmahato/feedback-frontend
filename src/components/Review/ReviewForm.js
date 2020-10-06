6c98f93410330dd94a59e02c52051978b6f7fc51
skmahato <souravkumar1329@gmail.com>
2020-10-06 17:23:45 +0530

    rating form changes

--- src/components/Review/ReviewForm.js
+++ src/components/Review/ReviewForm.js
@@ -5,12 +5,36 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    // marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const ReviewForm = ({ handleFormSubmit }) => {
     const [rating, setRating] = useState(0);
     const [title, setTitle] = useState('');
     const [comment, setComment] = useState('');
     const [visible, setVisible] = useState(true);
+    const classes = useStyles();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -24,40 +48,72 @@ const ReviewForm = ({ handleFormSubmit }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Review
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <TextField
-            label="Title"
             variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="title"
+            label="Title"
+            name="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-
-          <TextareaAutosize
-            rows={3}
-            placeholder="Comment"
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="comment"
+            label="Comment"
+            name="comment"
             value={comment}
             onChange={(e) => setComment(e.target.value)}
           />
-
-            <Rating
-                name="simple-controlled"
-                value={rating}
-                onChange={(event, newValue) => setRating(newValue)}
-            />
-
-       <Typography component="div">
-        <Grid component="label" container alignItems="center" spacing={1} label="Visibility">
-          <Grid item>Only Show To Admin</Grid>
-          <Grid item>
-            <AntSwitch checked={visible} onChange={() => setVisible(!visible)} name="checkedC" />
-          </Grid>
-          <Grid item>Public</Grid>
-        </Grid>
-      </Typography>
-
-
-          <Button type="submit">Submit</Button>
+          <Rating
+            name="simple-controlled"
+            value={rating}
+            onChange={(event, newValue) => setRating(newValue)}
+            required
+          />
+          <Typography component="div">
+            <Grid
+              component="label"
+              container
+              alignItems="center"
+              spacing={1}
+              label="Visibility"
+            >
+              <Grid item>Only Show To Admin</Grid>
+              <Grid item>
+                <AntSwitch
+                  checked={visible}
+                  onChange={() => setVisible(!visible)}
+                  name="checkedC"
+                />
+              </Grid>
+              <Grid item>Public</Grid>
+            </Grid>
+          </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
         </form>
+      </div>
+    </Container>
     )
 }
 
