37ae94965e401eebb150271e519793c6d8c7487b
Sourav Kumar Mahato <33520110+skmahato@users.noreply.github.com>
2020-10-06 18:53:04 +0530

    adding a deropdown column fr listing dealership (#7)
    Co-authored-by: Vijeta Sobhwani <VSobhwani@dealersocket.com>

---
+++ src/components/DealerInfo/DealershipSelect.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Select from "react-select";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+function DealershipSelect() {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  return <Select options={options}></Select>;
+}
+export default DealershipSelect;
--- src/components/DealerInfo/index.js
+++ src/components/DealerInfo/index.js
@@ -1,6 +1,6 @@
-import { blueGrey, green, grey, lightBlue } from '@material-ui/core/colors';
-import { flowRight } from 'lodash';
-import React from 'react';
+import React from "react";
+
+
 
 var divStyle = {
     backgroundColor: "whitesmoke",
@@ -10,16 +10,61 @@ var divStyle = {
 }
 
 export const DealerInfo = ({ dealer, handleDealerClick }) => {
+ 
     return (
-        <div style={divStyle}>
-            <strong>{dealer.name}</strong> | {dealer.location}
+        <div style={{ ...styles.DealershipRectangle }}>
+            <div><p style={{ ...styles.DealershipTitle }}><strong>{dealer.name}</strong></p> | {dealer.location}</div>
             <p>{dealer.description}</p>
             <p>{dealer.phone} | {dealer.email}</p>
             <em>{dealer.website}</em>
             <br />          
-            {handleDealerClick && <button type="button" onClick={() => handleDealerClick(dealer.id)} style={{backgroundColor: "grey"}}>Reviews</button>}
+            {handleDealerClick && <button style={{ ...styles.ReviewButtonStyle }} type="button" onClick={() => handleDealerClick(dealer.id)} ><p style = {{ ...styles.ButtonInnerText }} >Reviews</p></button>}
         </div>
     )
 }
 
 export default DealerInfo;
+
+
+const styles = {
+  ReviewButtonStyle: {
+    width: 100,
+  height: 44,
+  // boxShadow:'-2px 0px 0px 0px',
+  backgroundColor : '#009f0b',
+  border : 'solid 1px #dddddd', 
+  },
+  ButtonInnerText:
+  {   
+  fontFamily: "Roboto",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    // lineHeight: 3.07,
+  textAlign: 'center',
+  color: '#ffffff'
+  },
+  DealershipRectangle:{
+     backgroundColor: "#e6e6e6",
+    border : 'solid 1px #eaeaea', 
+    marginBottom: "5px",
+    padding: 5,
+  },
+  DealrshipTitle:{
+    
+    height: 18,
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "bold",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    lineHeight: 1.38,
+    letterSpacing: "normal",
+    color: "#1d86b8",
+
+  }
+
+
+  
+};
